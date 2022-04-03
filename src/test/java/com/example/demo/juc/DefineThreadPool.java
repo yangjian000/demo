60f343465b803ccac2ef5a3165458acd2d168830
yangj <347004884@qq.com>
2022-04-03 12:57:37 +0800

    自定义线程池

--- src/test/java/com/example/demo/juc/DefineThreadPool.java
+++ src/test/java/com/example/demo/juc/DefineThreadPool.java
@@ -10,20 +10,41 @@ import java.util.concurrent.TimeUnit;
 import java.util.concurrent.locks.Condition;
 import java.util.concurrent.locks.ReentrantLock;
 
-@Slf4j
+@Slf4j(topic = "c.DefineThreadPool")
 public class DefineThreadPool {
     public static void main(String[] args) {
-        ThreadPool pool = new ThreadPool(2, 1000, TimeUnit.MILLISECONDS, 10);
-        for (int i = 0; i < 15; i++) {
-            int j =i;
-            pool.execute(()->{
-                log.debug("{}",j);
+        ThreadPool pool = new ThreadPool(1, 1000, TimeUnit.MILLISECONDS, 1, (queue, task) -> {
+            // 1 等待
+//            queue.put(task);
+            // 2 超时等待
+//            queue.offer(task, 500, TimeUnit.MILLISECONDS);
+            // 3 放弃任务
+//            log.debug("放弃：{}",task);
+            // 4 抛出异常
+//            throw new RuntimeException("任务执行失败:" + task);
+            // 5 让调用者自己执行任务
+//            task.run();
+        });
+        for (int i = 0; i < 3; i++) {
+            int j = i;
+            pool.execute(() -> {
+                try {
+                    TimeUnit.SECONDS.sleep(1);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+                log.debug("{}", j);
             });
         }
     }
 }
 
+@FunctionalInterface
+interface RejectPolicy<T> {
+    void reject(BlockingQueue<T> taskQueue, T task);
+}
 
+@Slf4j(topic = "c.ThreadPool")
 class ThreadPool {
 
     BlockingQueue<Runnable> taskQueue;
@@ -36,26 +57,31 @@ class ThreadPool {
 
     private TimeUnit timeUnit;
 
+    private RejectPolicy<Runnable> rejectPolicy;
+
     public void execute(Runnable task) {
         synchronized (workers) {
             if (workers.size() < coreSize) {
                 Worker worker = new Worker(task);
+                log.debug("新增worker：{}", worker);
                 workers.add(worker);
                 worker.start();
             } else {
-                taskQueue.put(task);
+                taskQueue.tryPut(rejectPolicy, task);
             }
         }
     }
 
-    public ThreadPool(int coreSize, long timeout, TimeUnit timeUnit, int queueCapacity) {
+    public ThreadPool(int coreSize, long timeout, TimeUnit timeUnit, int queueCapacity, RejectPolicy rejectPolicy) {
         this.coreSize = coreSize;
         this.timeout = timeout;
         this.timeUnit = timeUnit;
+        this.rejectPolicy = rejectPolicy;
         this.taskQueue = new BlockingQueue<>(queueCapacity);
     }
 
     class Worker extends Thread {
+
         private Runnable task;
 
         public Worker(Runnable task) {
@@ -64,8 +90,10 @@ class ThreadPool {
 
         @Override
         public void run() {
-            while (task != null || (task = taskQueue.take()) != null) {
+//            while (task != null || (task = taskQueue.take()) != null) {
+            while (task != null || (task = taskQueue.poll(timeout, timeUnit)) != null) {
                 try {
+                    log.debug("正在执行：{}", task);
                     task.run();
                 } catch (Exception e) {
                     e.printStackTrace();
@@ -74,7 +102,8 @@ class ThreadPool {
                 }
             }
             synchronized (workers) {
-                workers.remove(task);
+                log.debug("worker 被移除：{}", this);
+                workers.remove(this);
             }
         }
     }
@@ -82,6 +111,7 @@ class ThreadPool {
 
 }
 
+@Slf4j(topic = "c.BlockingQueue")
 class BlockingQueue<T> {
 
     private Deque<T> deque = new ArrayDeque<>();
@@ -144,11 +174,13 @@ class BlockingQueue<T> {
         try {
             while (deque.size() == capacity) {
                 try {
+                    log.debug("等待加入任务队列：{}", element);
                     fullWaitSet.await();
                 } catch (InterruptedException e) {
                     e.printStackTrace();
                 }
             }
+            log.debug("加入任务队列：{}", element);
             deque.addLast(element);
             emptyWaitSet.signal();
         } finally {
@@ -156,23 +188,42 @@ class BlockingQueue<T> {
         }
     }
 
-    public void offer(T element,long timeout,TimeUnit timeUnit){
+    public boolean offer(T element, long timeout, TimeUnit timeUnit) {
         lock.lock();
         long nanos = timeUnit.toNanos(timeout);
         try {
-            while (deque.size() == capacity){
+            while (deque.size() == capacity) {
+                log.debug("等待加入任务队列：{}", element);
                 try {
-                    fullWaitSet.awaitNanos(nanos);
+                    if (nanos <= 0) {
+                        return false;
+                    }
+                    nanos = fullWaitSet.awaitNanos(nanos);
                 } catch (InterruptedException e) {
                     e.printStackTrace();
                 }
             }
+            log.debug("加入任务队列：{}", element);
             deque.addLast(element);
-            emptyWaitSet.notify();
-
-        }finally {
+            emptyWaitSet.signal();
+            return true;
+        } finally {
             lock.unlock();
         }
 
+    }
+
+    public void tryPut(RejectPolicy<T> rejectPolicy, T element) {
+        lock.lock();
+        try {
+            if (deque.size() == capacity) {
+                rejectPolicy.reject(this, element);
+            } else {
+                deque.addLast(element);
+                emptyWaitSet.signal();
+            }
+        } finally {
+            lock.unlock();
+        }
     }
 }
