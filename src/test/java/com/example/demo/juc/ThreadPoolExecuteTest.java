6702076970214706788e7a42fc48b19f7bfd5e08
yangj <347004884@qq.com>
2022-04-05 00:19:33 +0800

    executes

--- src/test/java/com/example/demo/juc/ThreadPoolExecuteTest.java
+++ src/test/java/com/example/demo/juc/ThreadPoolExecuteTest.java
@@ -4,20 +4,29 @@ import lombok.extern.slf4j.Slf4j;
 
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
+import java.util.concurrent.ThreadFactory;
+import java.util.concurrent.atomic.AtomicInteger;
 
 @Slf4j(topic = "c.ThreadPoolExecuteTest")
 public class ThreadPoolExecuteTest {
     public static void main(String[] args) {
-        ExecutorService pool = Executors.newFixedThreadPool(2);
+        ExecutorService pool = Executors.newFixedThreadPool(2, new ThreadFactory() {
+            private final AtomicInteger t = new AtomicInteger(1);
 
-        pool.execute(()->{
-          log.debug("{1}");
+            @Override
+            public Thread newThread(Runnable r) {
+                return new Thread(r,"my_pool" + t.getAndIncrement());
+            }
         });
-        pool.execute(()->{
-          log.debug("{2}");
+
+        pool.execute(() -> {
+            log.debug("{1}");
         });
-        pool.execute(()->{
-          log.debug("{3}");
+        pool.execute(() -> {
+            log.debug("{2}");
+        });
+        pool.execute(() -> {
+            log.debug("{3}");
         });
     }
 }
