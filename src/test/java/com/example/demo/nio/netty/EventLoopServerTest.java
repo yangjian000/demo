416ad866bc300d17c67f4c3ddfa811b489238a7e
yangj <347004884@qq.com>
2022-04-13 22:37:58 +0800

    Channel

--- src/test/java/com/example/demo/nio/netty/EventLoopServerTest.java
+++ src/test/java/com/example/demo/nio/netty/EventLoopServerTest.java
@@ -2,9 +2,7 @@ package com.example.demo.nio.netty;
 
 import io.netty.bootstrap.ServerBootstrap;
 import io.netty.buffer.ByteBuf;
-import io.netty.channel.ChannelHandlerContext;
-import io.netty.channel.ChannelInboundHandlerAdapter;
-import io.netty.channel.ChannelInitializer;
+import io.netty.channel.*;
 import io.netty.channel.nio.NioEventLoopGroup;
 import io.netty.channel.socket.nio.NioServerSocketChannel;
 import io.netty.channel.socket.nio.NioSocketChannel;
@@ -15,14 +13,24 @@ import java.nio.charset.Charset;
 @Slf4j
 public class EventLoopServerTest {
     public static void main(String[] args) {
-        new ServerBootstrap().group(new NioEventLoopGroup()).channel(NioServerSocketChannel.class).childHandler(new ChannelInitializer<NioSocketChannel>() {
+
+        EventLoopGroup group = new DefaultEventLoopGroup();
+
+        new ServerBootstrap().group(new NioEventLoopGroup(), new NioEventLoopGroup(2)).channel(NioServerSocketChannel.class).childHandler(new ChannelInitializer<NioSocketChannel>() {
             @Override
             protected void initChannel(NioSocketChannel ch) throws Exception {
-                ch.pipeline().addLast(new ChannelInboundHandlerAdapter() {
+                ch.pipeline().addLast("handler1", new ChannelInboundHandlerAdapter() {
                     @Override
                     public void channelRead(ChannelHandlerContext ctx, Object msg) throws Exception {
                         ByteBuf byteBuf = (ByteBuf) msg;
-                        byteBuf.toString(Charset.defaultCharset());
+                        log.debug(byteBuf.toString(Charset.defaultCharset()));
+                        ctx.fireChannelRead(msg);
+                    }
+                }).addLast(group, "handler2", new ChannelInboundHandlerAdapter() {
+                    @Override
+                    public void channelRead(ChannelHandlerContext ctx, Object msg) throws Exception {
+                        ByteBuf buf = (ByteBuf) msg;
+                        log.debug(buf.toString(Charset.defaultCharset()));
                     }
                 });
             }
--- src/test/java/com/example/demo/nio/netty/NettyTest01.java
+++ src/test/java/com/example/demo/nio/netty/NettyTest01.java
@@ -2,51 +2,62 @@ package com.example.demo.nio.netty;
 
 import io.netty.bootstrap.Bootstrap;
 import io.netty.bootstrap.ServerBootstrap;
-import io.netty.channel.Channel;
-import io.netty.channel.ChannelHandlerContext;
-import io.netty.channel.ChannelInboundHandlerAdapter;
-import io.netty.channel.ChannelInitializer;
+import io.netty.channel.*;
 import io.netty.channel.nio.NioEventLoopGroup;
 import io.netty.channel.socket.nio.NioServerSocketChannel;
 import io.netty.channel.socket.nio.NioSocketChannel;
 import io.netty.handler.codec.string.StringDecoder;
 import io.netty.handler.codec.string.StringEncoder;
+import lombok.extern.slf4j.Slf4j;
 
 import java.net.InetSocketAddress;
 
 public class NettyTest01 {
     public static void main(String[] args) {
-        new ServerBootstrap()
-                .group(new NioEventLoopGroup())
-                .channel(NioServerSocketChannel.class)
-                .childHandler(new ChannelInitializer<NioSocketChannel>() {
+        new ServerBootstrap().group(new NioEventLoopGroup()).channel(NioServerSocketChannel.class).childHandler(new ChannelInitializer<NioSocketChannel>() {
+            @Override
+            protected void initChannel(NioSocketChannel ch) throws Exception {
+                ch.pipeline().addLast(new StringDecoder());
+                ch.pipeline().addLast(new ChannelInboundHandlerAdapter() {
                     @Override
-                    protected void initChannel(NioSocketChannel ch) throws Exception {
-                        ch.pipeline().addLast(new StringDecoder());
-                        ch.pipeline().addLast(new ChannelInboundHandlerAdapter() {
-                            @Override
-                            public void channelRead(ChannelHandlerContext ctx, Object msg) throws Exception {
-                                System.out.println(msg);
-                            }
-                        });
+                    public void channelRead(ChannelHandlerContext ctx, Object msg) throws Exception {
+                        System.out.println(msg);
                     }
-                }).bind(8080);
+                });
+            }
+        }).bind(8080);
     }
 }
 
-class NettyTestClient{
+@Slf4j
+class NettyTestClient {
     public static void main(String[] args) throws InterruptedException {
-        Channel channel = new Bootstrap().
-                group(new NioEventLoopGroup())
-                .channel(NioSocketChannel.class)
-                .handler(new ChannelInitializer<NioSocketChannel>() {
+        // 带有 Future Promise 的类型都是和异步方法配套使用，用来处理结果
+        ChannelFuture channelFuture = new Bootstrap().group(new NioEventLoopGroup()).channel(NioSocketChannel.class).handler(new ChannelInitializer<NioSocketChannel>() {
                     @Override
                     protected void initChannel(NioSocketChannel ch) throws Exception {
                         ch.pipeline().addLast(new StringEncoder());
                     }
-                }).connect(new InetSocketAddress(8080))
-                .sync().channel();
-        System.out.println(channel);
+                })
+                // 异步非阻塞 main发起了调用，真正执行connect是nio线程
+                .connect(new InetSocketAddress(8080));
+
+        // 同步处理结果
+        channelFuture.sync();
+        Channel channel = channelFuture.channel();
+        channel.writeAndFlush("hello");
+        log.debug("{}", channel);
         System.out.println();
+
+        //
+        channelFuture.addListener(new ChannelFutureListener() {
+            @Override
+            public void operationComplete(ChannelFuture future) throws Exception {
+                Channel channel = future.channel();
+                log.debug("{}", channel);
+                channel.writeAndFlush("hello");
+            }
+        });
+
     }
 }
